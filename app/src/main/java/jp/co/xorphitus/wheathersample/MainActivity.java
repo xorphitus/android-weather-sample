ef97ac8f5e1726c2dc57ccfe5c45cd34a8c0a2b8
xorphitus <xorphitus@gmail.com>
2016-06-01 14:43:41 +0900

    メインコンテンツ表示用のFragmentを作成

--- app/src/main/java/jp/co/xorphitus/wheathersample/MainActivity.java
+++ app/src/main/java/jp/co/xorphitus/wheathersample/MainActivity.java
@@ -1,12 +1,24 @@
 package jp.co.xorphitus.wheathersample;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import jp.co.xorphitus.wheathersample.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      FragmentManager fragmentManager = getFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+      fragmentTransaction.add(R.id.container, new MainFragment());
+
+      fragmentTransaction.commit();
+    }
   }
 }
---
+++ app/src/main/java/jp/co/xorphitus/wheathersample/fragments/MainFragment.java
@@ -0,0 +1,19 @@
+package jp.co.xorphitus.wheathersample.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import jp.co.xorphitus.wheathersample.R;
+
+/**
+ * メインコンテンツ表示用
+ */
+public class MainFragment extends Fragment {
+  @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_main, container, false);
+  }
+}
