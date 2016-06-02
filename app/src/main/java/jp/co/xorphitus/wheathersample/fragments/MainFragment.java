0457d39e924b3a547b772b03b4d486b322a69401
xorphitus <xorphitus@gmail.com>
2016-06-02 19:24:45 +0900

    Rxの処理を手堅く

--- app/src/main/java/jp/co/xorphitus/wheathersample/fragments/MainFragment.java
+++ app/src/main/java/jp/co/xorphitus/wheathersample/fragments/MainFragment.java
@@ -13,12 +13,16 @@ import jp.co.xorphitus.wheathersample.network.LivedoorWeatherService;
 import jp.co.xorphitus.wheathersample.network.LivedoorWeatherServiceFactory;
 import rx.Observable;
 import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
  * メインコンテンツ表示用
  */
 public class MainFragment extends Fragment {
+  private Subscription weatherSubscription;
+
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     LivedoorWeatherService service = LivedoorWeatherServiceFactory.create();
@@ -26,21 +30,27 @@ public class MainFragment extends Fragment {
 
     final View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-    weather.subscribeOn(Schedulers.newThread()).subscribe(new Observer<Weather>() {
-      @Override public void onCompleted() {
+    weatherSubscription = weather.observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.newThread())
+        .subscribe(new Observer<Weather>() {
+          @Override public void onCompleted() {
 
-      }
+          }
 
-      @Override public void onError(Throwable e) {
-        e.printStackTrace();
-      }
+          @Override public void onError(Throwable e) {
+            e.printStackTrace();
+          }
 
-      @Override public void onNext(Weather weather) {
-        TextView textView = (TextView) view.findViewById(R.id.weather_text);
-        textView.setText(weather.title);
-      }
-    });
+          @Override public void onNext(Weather weather) {
+            TextView textView = (TextView) view.findViewById(R.id.weather_text);
+            textView.setText(weather.title);
+          }
+        });
 
     return view;
+  }
+
+  @Override public void onDestroy() {
+    weatherSubscription.unsubscribe();
   }
 }
